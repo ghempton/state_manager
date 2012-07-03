a0706d9e806cd76614416c7078121a379aa2183f
Gordon Hempton <ghempton@gmail.com>
2012-07-03 13:59:22 -0700

    Suport for psych seralization.

--- lib/state_manager/serialization.rb
+++ lib/state_manager/serialization.rb
@@ -1,13 +1,31 @@
-module StateManager
-  class Base
-    yaml_as "tag:grouptalent.com,2012:StateManager"
+require 'yaml'
+if YAML.parser.class.name =~ /syck|yecht/i
+  module StateManager
+    class Base
+      yaml_as "tag:grouptalent.com,2012:StateManager"
 
-    def self.yaml_new(klass, tag, val)
-      klass.new(val['resource'], val['context'])
+      def self.yaml_new(klass, tag, val)
+        klass.new(val['resource'], val['context'])
+      end
+
+      def to_yaml_properties
+        ['@resource', '@context']
+      end
     end
+  end
+else
+  module StateManager
+    class Base
+      def encode_with(coder)
+        coder.map = {
+          "resource" => resource,
+          "context" => context
+        }
+      end
 
-    def to_yaml_properties
-      ['@resource', '@context']
+      def init_with(coder)
+        initialize(coder["resource"], coder["context"])
+      end
     end
   end
 end
